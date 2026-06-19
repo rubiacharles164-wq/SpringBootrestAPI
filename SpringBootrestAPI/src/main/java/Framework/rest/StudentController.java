c375250bf0773d48ea5f5b1409c86103ca08543d
rubiacharles164-wq <rubiacharles164@gmail.com>
2026-06-19 10:08:30 -0230

    Add files via upload

---
+++ SpringBootrestAPI/src/main/java/Framework/rest/StudentController.java
@@ -0,0 +1,39 @@
+package Framework.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class StudentController {
+    @Autowired
+    private StudentService studentservice;
+
+    @GetMapping("/students")
+    public  List<Student> getAllStudents(){
+        return studentservice.getAllStudents();
+    }
+
+    @GetMapping("/student/{id}")
+    public Student getStudentById(@PathVariable long id) {
+        return studentservice.getStudentById(id);
+    }
+
+    @PostMapping("/student")
+    public Student createStudent(@RequestBody Student student) {
+        return studentservice.createStudent(student);
+    }
+
+    @PutMapping("/student/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        return ResponseEntity.ok(studentservice.updateStudent(id, student));
+    }
+
+    @DeleteMapping("/student/{id}")
+    public void deleteStudentById(@PathVariable Long id) {
+        studentservice.deleteStudentById(id);
+    }
+}
---
+++ SpringBootrestAPI/src/main/java/Framework/rest/StudentService.java
@@ -0,0 +1,50 @@
+package Framework.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getAllStudents(){
+        return (List<Student>) studentRepository.findAll();
+    }
+
+    public Student getStudentById(long id) {
+        Optional<Student> SchoolOptional = studentRepository.findById(id);
+
+        return SchoolOptional.orElse(null);
+    }
+
+    public void deleteStudentById(long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Student createStudent(Student newStudent) {
+        return studentRepository.save(newStudent);
+    }
+
+    public Student updateStudent(long id, Student updatedStudent) {
+        Optional<Student> studentToUpdateOptional = studentRepository.findById(id);
+
+        if (studentToUpdateOptional.isPresent()) {
+            Student studentToUpdate = studentToUpdateOptional.get();
+
+            studentToUpdate.setName(updatedStudent.getName());
+            studentToUpdate.setEmail(updatedStudent.getEmail());
+            studentToUpdate.setCourse(updatedStudent.getCourse());
+            studentToUpdate.setGrade(updatedStudent.getGrade());
+            studentToUpdate.setPassing(updatedStudent.getPassing());
+
+            return studentRepository.save(studentToUpdate);
+        }
+
+        return null;
+    }
+
+}
